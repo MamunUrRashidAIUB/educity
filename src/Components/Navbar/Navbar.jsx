043fa2d142d1798e6fc8889a14d7e67e8a8b8e8f
Mamun ur rashid <md.mamun.ur.rashid.cse@gmail.com>
2024-10-05 21:27:48 +0600

    navbar created and style changes

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,18 +1,23 @@
-import React from 'react'
-import './Navbar.css'
-import logo from '../../assets/logo.png'
+import React from 'react';
+import './Navbar.css';
+import logo from '../../assets/logo.png';
+
 const Navbar = () => {
   return (
-    <nav>
-        <img src={logo} alt="" />
-        <ul><li>Home</li>
+    <nav className="navbar">
+      <img src={logo} alt="Website Logo" className="navbar-logo" />
+      <ul className="navbar-menu">
+        <li>Home</li>
         <li>Program</li>
-        <li>About us</li>
+        <li>About Us</li>
         <li>Campus</li>
         <li>Testimonials</li>
-        <li>Contact us</li></ul>
+        <li>
+          <button className="contact-button">Contact Us</button>
+        </li>
+      </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
