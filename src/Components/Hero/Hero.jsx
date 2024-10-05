7b67c6b90af328323c8855764141e56ab1e299e9
Mamun ur rashid <md.mamun.ur.rashid.cse@gmail.com>
2024-10-05 22:57:13 +0600

    hero section text added

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import './Hero.css'
 const Hero = () => {
-  return (
-    <div className='hero'></div>
-  )
+    return (
+        <div className='hero'>
+<h1>We Ensure better education for a better world</h1>
+<p>Our cutting-edge curriculum is designed to empower students with the knowledge, skills, and experiences needed to excel in the dynamic field of education</p>
+        </div>
+    )
 }
 
 export default Hero
