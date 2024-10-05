20d871e76d4b43bc0a0c8c1580d474d364578046
Mamun ur rashid <md.mamun.ur.rashid.cse@gmail.com>
2024-10-06 01:30:49 +0600

    about section added

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
 import Programs from './Components/Programs/Programs'
 import Title from './Components/Title/Title'
+import About from './Components/About/About'
 
 const App = () => {
   return (
     <div><Navbar/>
     <Hero/>
     <Title/>
-    <Programs/></div>
+    <Programs/>
+    <About/></div>
   )
 }
 
---
+++ src/Components/About/About.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './About.css';
+import about_img from '../../assets/about.png';
+import play_icon from '../../assets/play-icon.png';
+
+const About = () => {
+    return (
+        <div className='about-container'>
+            <div className='about-images'>
+                <img src={about_img} className='about_image' alt="About University" />
+                <img src={play_icon} className='play_icon' alt="Play Video" />
+            </div>
+            <div className='about-text'>
+                <h3>About University</h3>
+                <h2>Nurturing Tomorrow's Leaders Today</h2>
+                <p>
+                    Embark on a transformative educational journey with our university's comprehensive education programs. Our cutting-edge curriculum is designed to empower students with the knowledge, skills, and experiences needed to excel in the dynamic field of education.
+
+                    With a focus on innovation, hands-on learning, and personalized mentorship, our programs prepare aspiring educators to make a meaningful impact in classrooms, schools, and communities.
+
+                    Whether you aspire to become a teacher, administrator, counselor, or educational leader, our diverse range of programs offers the perfect pathway to achieve your goals and unlock your full potential in shaping the future of education.
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default About;
