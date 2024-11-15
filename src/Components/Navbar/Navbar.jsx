d2484a1bd430986f0a675998da6d75b0ef3aeca4
Md. Mamun ur rashid <md.mamun.ur.rashid.cse@gmail.com>
2024-11-16 02:13:03 +0600

    navbar changes

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,60 +1,30 @@
-import React, { useState } from "react";
-import "./Navbar.css";
-import logo from "../../assets/logo.png";
-import { Link } from "react-scroll";
+import React, { useEffect, useState } from 'react';
+import './Navbar.css';
+import logo from '../../assets/logo.png';
+import { Link } from 'react-scroll';
+
 
 const Navbar = () => {
-  const [menuActive, setMenuActive] = useState(false);
 
-  const toggleMenu = () => {
-    setMenuActive(!menuActive);
-  };
-
+// const [sticky,setsticky]=useState(false);
+// useEffect(()=>{window.addEventListener('scroll',()=>{
+//   window.scrollY > 50 ? setsticky(true):setsticky(false);
+// })},[]);
   return (
-    <nav className={"navbar"}>
+    <nav className={'navbar'}>
       <img src={logo} alt="Website Logo" className="navbar-logo" />
-      <ul className={`navbar-menu ${menuActive ? "active" : ""}`}>
-        <li>
-          <Link to="hero" smooth={true} offset={0} duration={500}>
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link to="programs" smooth={true} offset={-230} duration={500}>
-            Program
-          </Link>
-        </li>
-        <li>
-          <Link to="about-container" smooth={true} offset={-80} duration={500}>
-            About us
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="photos-container"
-            smooth={true}
-            offset={-190}
-            duration={500}
-          >
-            Campus
-          </Link>
-        </li>
-        <li>
-          <Link to="testimonial" smooth={true} offset={-230} duration={500}>
-            Testimonials
-          </Link>
-        </li>
+      <ul className="navbar-menu">
+        <li> <Link to='hero' smooth={true} offset={0} duration={500}>Home</Link>  </li>
+        <li> <Link to='programs' smooth={true} offset={-230} duration={500}>Program</Link> </li>
+        <li><Link to='about-container' smooth={true} offset={-80} duration={500}>About us</Link> </li>
+        <li><Link to='photos-container' smooth={true} offset={-190} duration={500}>Campus</Link></li>
+        <li><Link to='testimonial' smooth={true} offset={-230} duration={500}>Testimonials</Link></li>
         <li className="contact-button">
           <Link to="contact-info" smooth={true} offset={-100} duration={500}>
-            Contact Us
+              Contact Us
           </Link>
         </li>
       </ul>
-      <div className="hamburger" onClick={toggleMenu}>
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
     </nav>
   );
 };
