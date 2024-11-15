850ca16a0b559753079de57516faa6bcd8d5e77c
Md. Mamun ur rashid <md.mamun.ur.rashid.cse@gmail.com>
2024-11-16 02:13:37 +0600

    some change

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,27 +1,47 @@
-import React, { useEffect, useState } from 'react';
-import './Navbar.css';
-import logo from '../../assets/logo.png';
-import { Link } from 'react-scroll';
-
+import "./Navbar.css";
+import logo from "../../assets/logo.png";
+import { Link } from "react-scroll";
 
 const Navbar = () => {
-
-// const [sticky,setsticky]=useState(false);
-// useEffect(()=>{window.addEventListener('scroll',()=>{
-//   window.scrollY > 50 ? setsticky(true):setsticky(false);
-// })},[]);
   return (
-    <nav className={'navbar'}>
+    <nav className={"navbar"}>
       <img src={logo} alt="Website Logo" className="navbar-logo" />
       <ul className="navbar-menu">
-        <li> <Link to='hero' smooth={true} offset={0} duration={500}>Home</Link>  </li>
-        <li> <Link to='programs' smooth={true} offset={-230} duration={500}>Program</Link> </li>
-        <li><Link to='about-container' smooth={true} offset={-80} duration={500}>About us</Link> </li>
-        <li><Link to='photos-container' smooth={true} offset={-190} duration={500}>Campus</Link></li>
-        <li><Link to='testimonial' smooth={true} offset={-230} duration={500}>Testimonials</Link></li>
+        <li>
+          {" "}
+          <Link to="hero" smooth={true} offset={0} duration={500}>
+            Home
+          </Link>{" "}
+        </li>
+        <li>
+          {" "}
+          <Link to="programs" smooth={true} offset={-230} duration={500}>
+            Program
+          </Link>{" "}
+        </li>
+        <li>
+          <Link to="about-container" smooth={true} offset={-80} duration={500}>
+            About us
+          </Link>{" "}
+        </li>
+        <li>
+          <Link
+            to="photos-container"
+            smooth={true}
+            offset={-190}
+            duration={500}
+          >
+            Campus
+          </Link>
+        </li>
+        <li>
+          <Link to="testimonial" smooth={true} offset={-230} duration={500}>
+            Testimonials
+          </Link>
+        </li>
         <li className="contact-button">
           <Link to="contact-info" smooth={true} offset={-100} duration={500}>
-              Contact Us
+            Contact Us
           </Link>
         </li>
       </ul>
