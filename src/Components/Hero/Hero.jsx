ce3fa8cf6b77ccb0549a15dffb42275f213b444f
Mamun Ur Rashid <you@example.com>
2025-03-09 22:58:22 +0600

    cmnt

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
-import './Hero.css';
+import React from "react";
+import "./Hero.css";
 
 const Hero = () => {
-    return (
-        <div className='hero'>
-            <h1>We Ensure better education for a better world</h1>
-            <p>Our cutting-edge curriculum is designed to empower students with the knowledge, skills, and experiences needed to excel in the dynamic field of education</p>
-            <button className="explore-button">Explore more</button>
-        </div>
-    );
-}
+  return (
+    <div className="hero">
+      <h1>We Ensure better education for a better world</h1>
+      <p>
+        Our cutting-edge curriculum is designed to empower students with the
+        knowledge, skills, and experiences needed to excel in the dynamic field
+        of education
+      </p>
+      <button className="explore-button">Explore more</button>
+    </div>
+  );
+};
+//need more detail
 
 export default Hero;
